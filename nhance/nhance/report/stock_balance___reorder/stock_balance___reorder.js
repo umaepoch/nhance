edd49b3d140eb5af6c46dfbb1257ee1fcde72d7c
Uma <umag@epochconsulting.in>
2017-09-01 17:15:15 +0530

    Reordering Cycle

---
+++ nhance/nhance/report/stock_balance___reorder/stock_balance___reorder.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Epoch and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Stock Balance - Reorder"] = {
+	"filters": [
+
+	]
+}
