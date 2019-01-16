c567ef1c7fb98b074a1d64624ac824ee361f43a7
umaepoch <umag@epochconsulting.in>
2019-01-16 12:42:03 +0530

    Nhance Updates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('requirements.txt') as f:
 install_requires = f.read().strip().split('\n')
 
-with open('nhance/init.py', 'rb') as f:
+with open('nhance/__init__.py', 'rb') as f:
 version = str(ast.literal_eval(_version_re.search(
 f.read().decode('utf-8')).group(1)))
 
