9eaed0e1364755f43d1672ba1cb466053816ac25
umaepoch <umaepoch@consulting.in>
2019-08-08 10:15:13 +0530

    FG Dispatch Report

---
+++ nhance/nhance/report/fg_dispatch_report/fg_dispatch_report.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2016, Epoch and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["FG Dispatch Report"] = {
+	"filters": [{
+        "fieldname": "company",
+        "label": __("Company"),
+        "fieldtype": "Link",
+        "options": "Company",
+        "reqd": 1
+	},
+       {
+        "fieldname": "sales_order",
+        "label": __("Sales Order"),
+        "fieldtype": "Link",
+        "options": "Sales Order",
+        "reqd": 1,
+        "get_query": function() {
+	           return {
+                    "doctype": "Sales Order",
+		     "filters" : [
+			['Sales Order', 'docstatus', '!=', '2']
+			]
+              }
+         }
+}
+]
+}
