9219a628ca0910df39e91ff18b2384508af544ff
gujjarijyoti <jyotigujjari1996@gmail.com>
2019-12-12 15:52:27 +0530

    API for Stock Entry

--- nhance/api.py
+++ nhance/api.py
@@ -1733,4 +1733,12 @@ def cancel_stock_entry_material_receipt(pch_ste_pull_short_rm):
     frappe.msgprint("The Stock Entry is cancelled successfully!!")
     return 1
 
+#jyoti
+@frappe.whitelist()
+def get_serial_number_details(duplicate_serial):
+    #print "coming inside get_serial_number_details---"
+    serial_no_list = frappe.db.sql("""select max(serial_no) as serial_no from `tabSerial No` where serial_no like '"""+duplicate_serial+"%""'""", as_dict=1)
+    #print "serial_no_list----",serial_no_list
+    return serial_no_list
+
 
