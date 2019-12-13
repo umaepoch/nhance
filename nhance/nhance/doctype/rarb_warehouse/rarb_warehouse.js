2278aa22a603c1c9c024ef2f8a10da595d6c3817
umaepoch <umag@epochconsulting.in>
2019-12-13 15:43:33 +0530

    nhance update

---
+++ nhance/nhance/doctype/rarb_warehouse/rarb_warehouse.js
@@ -0,0 +1,265 @@
+// Copyright (c) 2019, Epoch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('RARB Warehouse', {
+	onload: function(frm) {
+		cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_type", true)
+		if (cur_frm.doc.__islocal){
+			if(cur_frm.doc.amended_from == undefined){
+				cur_frm.set_df_property("rarbs", "hidden", true);
+			}
+			else if(cur_frm.doc.amended_from != undefined){
+				cur_frm.set_df_property("make_rarbs", "hidden", true);
+			}
+		}
+		if(cur_frm.doc.warehouse != undefined){
+			cur_frm.set_df_property("warehouse", "read_only", true);
+		}
+		//console.log("current date -----------"+cur_frm.doc.start_date);
+		if (!cur_frm.doc.__islocal){
+			
+				
+			
+			//console.log("end date from new changes-----------"+cur_frm.doc.end_date);
+			var today = new Date();
+			//console.log("today -------------"+today);
+			//var cur_date = today.getDate();
+			var cur_date_py = "";
+			if(cur_frm.doc.end_date != undefined && cur_frm.doc.end_date != ""){
+				var higher_date = get_higher_date(cur_frm.doc.warehouse,cur_frm.doc.start_date,cur_frm.doc.name);
+				if(higher_date != undefined){
+					console.log("higher_date--------------"+higher_date);
+					cur_frm.set_value("end_date", higher_date);
+				}
+				else{
+					console.log("hieger date is not coming");
+					cur_frm.set_value("end_date", "");
+				}
+				//console.log("higher_date----------------"+higher_date);
+				cur_date_py = get_current_date_format(cur_frm.doc.end_date, cur_frm.doc.start_date, cur_frm.doc.warehouse);
+				if(cur_date_py == true){
+				    if(cur_frm.doc.docstatus ==1){
+					cur_frm.set_value("is_active", 1);
+				    }
+				     else if(cur_frm.doc.docstatus == 2){
+						cur_frm.set_value("is_active", 0);
+					}
+				}else if (cur_date_py == false){
+					cur_frm.set_value("is_active", 0);
+				}
+				
+			}else{
+				var higher_date = get_higher_date(cur_frm.doc.warehouse,cur_frm.doc.start_date,cur_frm.doc.name);
+				if(higher_date != undefined){
+					cur_frm.set_value("end_date", higher_date);
+				}
+				else{
+					cur_frm.set_value("end_date", "");
+				}
+				var next_start_date = get_next_start_date(cur_frm.doc.warehouse,cur_frm.doc.start_date,cur_frm.doc.name);
+				console.log("next_start_date------------"+next_start_date);
+				if(next_start_date == true){
+				     if(cur_frm.doc.docstatus ==1){
+					cur_frm.set_value("is_active", 1);
+					}
+				     else if(cur_frm.doc.docstatus == 2){
+						cur_frm.set_value("is_active", 0);
+					}
+				}else if (next_start_date == false){
+					cur_frm.set_value("is_active", 0);
+				}
+			}
+				}
+		
+	},
+	make_rarbs : function(){
+		cur_frm.set_df_property("rarbs", "hidden", false);
+		for(var i = 1; i <= cur_frm.doc.number_of_rooms; i++){
+			for(var j = 1; j <=  cur_frm.doc.number_of_aisles_per_room; j++){
+				for(var k =1; k <= cur_frm.doc.number_of_racks_per_aisle; k++){
+					for(var l = 1; l <= cur_frm.doc.number_of_bins_per_rack; l++){
+						var rarb_id = i+"-"+j+"-"+k+"-"+l;
+						console.log(rarb_id);
+						var child = cur_frm.add_child("rarbs");
+				                frappe.model.set_value(child.doctype, child.name, "rarb_id", rarb_id);
+					}
+				}
+			}
+		cur_frm.refresh_field('rarbs');
+		}
+		cur_frm.set_df_property("make_rarbs", "hidden", true);
+	}
+		
+});
+frappe.ui.form.on('RARB Warehouse', "on_submit", function(frm , cdt , cdn){
+	//console.log("submitted-----------------");
+	
+	var update_previouse_doc_status = get_update_pre_doc(cur_frm.doc.warehouse, cur_frm.doc.start_date, cur_frm.doc.name);
+});
+frappe.ui.form.on("RARB Warehouse" ,{
+	is_active: function(frm){
+		if(cur_frm.doc.docstatus = 1){
+			var update_doc_end_date_is_active = get_update_is_active(cur_frm.doc.name, cur_frm.doc.is_active);
+		}
+		if(cur_frm.doc.docstatus == 0){
+			cur_frm.save();
+		}
+	}
+
+});
+frappe.ui.form.on("RARB Warehouse","onload", function(frm, cdt, cdn){
+	$.each(frm.doc.rarbs, function(i, item) {
+		console.log("rarb active ---------"+item.rarb_active);
+		if(item.rarb_active == "Yes"){
+			 cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_type", true)
+			}
+		else{
+			 cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_type", false)
+		}
+	})
+	frappe.ui.form.on("RARB Warehouse Item", {
+
+		 rarb_active: function (frm, cdt, cdn){
+			console.log("hello-------------");
+			var d = locals[cdt][cdn];
+			if(d.rarb_active == "Yes"){
+				 cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_type", true)
+				}
+			else if(d.rarb_active == "No"){
+				 cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_type", false)
+			}
+		},
+		rarb_type: function(frm ,cdt ,cdn){
+			var d = locals[cdt][cdn];
+			if(d.rarb_type == "Specific Item"){
+				 cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_item", true)
+				}
+			else if(d.rarb_type == "Any Item"){
+				console.log("rarb type is not specific item");
+				 cur_frm.fields_dict.rarbs.grid.toggle_reqd("rarb_item", false)
+			}
+		}
+	});
+});
+function get_higher_date(warehouse,start_date,name){
+	var supplier_criticality = "";
+	  frappe.call({
+        method: 'nhance.nhance.doctype.rarb_warehouse.rarb_warehouse.get_higher_date',
+        args: {
+	    "warehouse":warehouse,
+            "start_date":start_date,
+	    "name":name
+        },
+        async: false,
+        callback: function(r) {
+            console.log("supplier criticality..." + JSON.stringify(r.message));
+           supplier_criticality = r.message;
+	   // end_date = (new Date(supplier_criticality-1));
+	  // console.log("end date -------------"+supplier_criticality);
+        }
+    });
+    return supplier_criticality;
+
+
+}
+function get_current_date_format(end_date,start_date,warehouse){
+	var supplier_criticality = "";
+	  frappe.call({
+        method: 'nhance.nhance.doctype.rarb_warehouse.rarb_warehouse.get_end_foramte_date',
+        args: {
+	    "end_date":end_date,
+	    "start_date":start_date,
+	    "warehouse": warehouse
+        },
+        async: false,
+        callback: function(r) {
+            console.log("supplier criticality..." + JSON.stringify(r.message));
+           supplier_criticality = r.message;
+	   // end_date = (new Date(supplier_criticality-1));
+	  // console.log("end date -------------"+supplier_criticality);
+        }
+    });
+    return supplier_criticality;
+
+
+}
+function get_next_start_date(warehouse,start_date,name){
+	console.log("no end date");
+	var supplier_criticality = "";
+	  frappe.call({
+        method: 'nhance.nhance.doctype.rarb_warehouse.rarb_warehouse.get_next_start_date',
+        args: {
+	  "start_date":start_date,
+	   "warehouse":warehouse,
+	   "name":name
+        },
+        async: false,
+        callback: function(r) {
+            console.log("supplier criticality..." + JSON.stringify(r.message));
+           supplier_criticality = r.message;
+	   // end_date = (new Date(supplier_criticality-1));
+	  // console.log("current date------------"+supplier_criticality);
+        }
+    });
+    return supplier_criticality;
+
+
+}
+function get_update_pre_doc(warehouse,start_date,name){
+	var supplier_criticality = "";
+	  frappe.call({
+        method: 'nhance.nhance.doctype.rarb_warehouse.rarb_warehouse.get_update_pre_doc',
+        args: {
+	  "start_date":start_date,
+	   "warehouse":warehouse,
+	   "name":name
+        },
+        async: false,
+        callback: function(r) {
+            console.log("supplier criticality..." + JSON.stringify(r.message));
+           supplier_criticality = r.message;
+	   // end_date = (new Date(supplier_criticality-1));
+	//   console.log("current date------------"+supplier_criticality);
+        }
+    });
+    return supplier_criticality;
+
+
+}
+function get_update_doc(name,end_date){
+	var supplier_criticality = "";
+	  frappe.call({
+        method: 'nhance.nhance.doctype.rarb_warehouse.rarb_warehouse.get_update_doc',
+        args: {
+	   "name":name,
+	   "end_date":end_date
+        },
+        async: false,
+        callback: function(r) {
+            console.log("supplier criticality..." + JSON.stringify(r.message));
+           supplier_criticality = r.message;
+	   // end_date = (new Date(supplier_criticality-1));
+	//   console.log("current date------------"+supplier_criticality);
+        }
+    });
+    return supplier_criticality;
+}
+function get_update_is_active(name,is_active){
+	var supplier_criticality = "";
+	  frappe.call({
+        method: 'nhance.nhance.doctype.rarb_warehouse.rarb_warehouse.get_update_is_active',
+        args: {
+	   "name":name,
+	   "is_active":is_active
+        },
+        async: false,
+        callback: function(r) {
+            console.log("supplier criticality..." + JSON.stringify(r.message));
+           supplier_criticality = r.message;
+	   // end_date = (new Date(supplier_criticality-1));
+	  console.log("current date------------"+supplier_criticality);
+        }
+    });
+    return supplier_criticality;
+
+}
---
+++ nhance/nhance/doctype/rarb_warehouse/rarb_warehouse.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Epoch and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
+from datetime import datetime, timedelta
+import datetime
+class RARBWarehouse(Document):
+	def on_cancel(self):
+		frappe.db.set(self, "is_active", 0)
+
+@frappe.whitelist()
+def make_rarb_warehouse(source_name, target_doc=None, ignore_permissions=False):
+	#print "source_name ------------",source_name
+	def set_missing_values(source, target):
+		target.is_pos = 0
+		target.ignore_pricing_rule = 1
+		target.flags.ignore_permissions = True
+		target.run_method("set_missing_values")
+		target.run_method("set_po_nos")
+		target.run_method("calculate_taxes_and_totals")
+#		set company address
+#		target.update(get_company_address(target.company))
+#		if target.company_address:
+#			target.update(get_fetch_values("Documents Review Details", 'company_address', target.company_address))
+#		 set the redeem loyalty points if provided via shopping cart
+#		if source.loyalty_points and source.order_type == "Shopping Cart":
+#			target.redeem_loyalty_points = 1
+	def postprocess(source, target):
+		set_missing_values(source, target)
+		#Get the advance paid Journal Entries in Sales Invoice Advance
+		#target.set_advances()
+	doc = get_mapped_doc("Warehouse", source_name,	{
+		"Warehouse": {
+			"doctype": "RARB Warehouse",
+			"field_map": {
+				"warehouse": source_name
+			},
+			"validation": {
+				"docstatus": ["=", 0],
+			}
+		}
+	},  target_doc, postprocess, ignore_permissions=ignore_permissions)
+
+	return doc
+@frappe.whitelist()
+def get_higher_date(warehouse,start_date,name):
+	end_date = ""
+	start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+	#print "start_date----------------",start_date
+	dates = frappe.db.sql("""select name,start_date from `tabRARB Warehouse` where warehouse = %s and start_date > %s and docstatus =1 order by start_date asc limit 1 """,(warehouse,start_date), as_dict = 1)
+	#print "dates----------------",dates
+	dates_lower = frappe.db.sql("""select name,start_date from `tabRARB Warehouse` where warehouse = %s and start_date < %s and docstatus = 1 order by start_date desc limit 1 """,(warehouse,start_date), as_dict = 1)
+	#print "dates_lower---------------",dates_lower
+	if dates_lower:
+		for date in dates:
+			end_date = start_date - timedelta(days=1)
+			names = date.name
+			#get_update_doc_value(names,end_date)
+			frappe.db.sql("""update `tabRARB Warehouse` set end_date = '"""+ str(end_date)+"""'where name='"""+str(names)+"""'""")
+	if dates:
+		#end_date = []
+		for date in dates:
+			
+			end_date = date.start_date - timedelta(days=1)
+			#get_update_doc_value(name,end_date)
+			frappe.db.sql("""update `tabRARB Warehouse` set end_date = '"""+ str(end_date)+"""'where name='"""+str(name)+"""'""")
+			#print "end_date--------------",end_date
+		#print "nearest_date------------",nearest_date
+		return end_date
+	else:
+		
+		frappe.db.sql("""update `tabRARB Warehouse` set end_date = Null where name='"""+str(name)+"""' and docstatus =1""")
+		return None
+
+@frappe.whitelist()
+def get_end_foramte_date(end_date,start_date, warehouse):
+	flag = False
+	now = datetime.datetime.now()
+	current_date =  now.strftime("%Y-%m-%d")
+	
+	#higher_date = get_higher_date(warehouse,start_date)
+	if current_date <= end_date and current_date >= start_date:
+		#print "i am coming here"
+		is_active = 1
+		flag = True
+		#frappe.db.sql("""update `tabRARB Warehouse` set is_active = '"""+ str(is_active)+"""'where warehouse='"""+str(warehouse)+"""'""")
+	else:
+		#print "or i am coming here"
+		is_active = 0
+		flag = False
+		#frappe.db.sql("""update `tabRARB Warehouse` set is_active = '"""+ str(is_active)+"""'where warehouse='"""+str(warehouse)+"""'""")
+	return flag
+@frappe.whitelist()
+def get_next_start_date(warehouse,start_date,name):
+	flag = False
+	get_higher_date = ""
+	now = datetime.datetime.now()
+	current_date =  now.strftime("%Y-%m-%d")
+	#print "current_date------------",type(current_date)
+	get_higher_date = dates = frappe.db.sql("""select name,start_date from `tabRARB Warehouse` where warehouse = %s and start_date > %s and docstatus =1 order by start_date asc limit 1 """,(warehouse,start_date), as_dict = 1)
+	if start_date <= current_date:
+		#print "yeah date is greater then"
+		is_active = 1
+		flag = True
+		#frappe.db.sql("""update `tabRARB Warehouse` set is_active = '"""+ str(is_active)+"""' where warehouse='"""+str(warehouse)+"""' and name = '"""+name+"""'""")
+	elif start_date <= current_date and get_higher_date is None:
+		flag = True
+	elif start_date > current_date:
+		is_active = 0
+		flag = False
+		#frappe.db.sql("""update `tabRARB Warehouse` set is_active = '"""+ str(is_active)+"""' where warehouse='"""+str(warehouse)+"""' and name = '"""+name+"""'""")
+	#return current_date
+	return flag
+
+@frappe.whitelist()
+def get_update_pre_doc(warehouse,start_date,name):
+	get_is_active_update(warehouse,start_date,name)
+	start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+	dates = frappe.db.sql("""select name,start_date from `tabRARB Warehouse` where warehouse = %s and start_date < %s and docstatus =1 order by start_date desc limit 1 """,(warehouse,start_date), as_dict = 1)
+	dates_higher = frappe.db.sql("""select name,start_date from `tabRARB Warehouse` where warehouse = %s and start_date > %s and docstatus =1 order by start_date asc limit 1 """,(warehouse,start_date), as_dict = 1)
+	#print "dates----------------",dates
+	if dates_higher:
+		for date in dates_higher:
+			end_date = date.start_date - timedelta(days=1)
+			get_update_doc_value(name,end_date)
+			frappe.db.sql("""update `tabRARB Warehouse` set end_date = '"""+ str(end_date)+"""' where name='"""+str(name)+"""' and docstatus =1""")
+	if dates:
+		for date in dates:
+			end_date = start_date - timedelta(days=1)
+			names = date.name
+			#get_update_doc_value(names,end_date)
+			frappe.db.sql("""update `tabRARB Warehouse` set end_date = '"""+ str(end_date)+"""' where name='"""+str(names)+"""'""")
+	get_is_active_update(warehouse,start_date,name)
+	return dates
+
+def get_is_active_update(warehouse,start_date,name):
+
+	dates = frappe.db.sql("""select name,start_date,end_date from `tabRARB Warehouse` where warehouse = %s and start_date < %s and docstatus =1 order by start_date desc limit 1 """,(warehouse,start_date), as_dict = 1)
+	
+	now = datetime.datetime.now()
+	current_date =  now.strftime("%Y-%m-%d")
+	#current_date = datetime.datetime.strptime(current_date, '%Y-%m-%d')
+	for date in dates:
+		names = date.name
+		previous_start_date = date.start_date
+		previous_end_date = date.end_date
+		#print "type(previous_start_date) ----------",type(previous_start_date)
+		#print "type(previous_end_date) ----------",type(previous_end_date)
+		#print "type(current_date) ----------",type(current_date)
+		if str(previous_start_date) <= current_date and str(previous_end_date) >= current_date:
+			#print "yeah date is greater then"
+			is_active = 1
+			#flag = True
+			#print "is_active===============",is_active
+			frappe.db.sql("""update `tabRARB Warehouse` set is_active = '"""+ str(is_active)+"""' where warehouse='"""+str(warehouse)+"""' and name = '"""+names+"""'""")
+		else :
+			is_active = 0
+			#print "is_active===============",is_active
+			#flag = False
+			frappe.db.sql("""update `tabRARB Warehouse` set is_active = '"""+ str(is_active)+"""' where warehouse='"""+str(warehouse)+"""' and name = '"""+names+"""'""")
+	return True
+@frappe.whitelist()
+def get_update_doc(name,end_date):
+	doc = frappe.get_doc("RARB Warehouse" , name)
+	modified = doc.modified
+	doc.update({
+		"modified":modified,
+		"end_date":end_date	
+		})
+	doc.save()
+	return True
+@frappe.whitelist()
+def get_update_is_active(name,is_active):
+	doc = frappe.get_doc("RARB Warehouse" , name)
+	modified = doc.modified
+	doc.update({
+		   "modified":modified,
+		   "is_active":is_active
+		})
+	doc.save()
+	return True
+@frappe.whitelist()
+def get_update_doc_value(name,end_date):
+	doc = frappe.get_doc("RARB Warehouse" , name)
+	modified = doc.modified
+	doc.update({
+		"modified":modified,
+		"end_date":end_date	
+		})
+	frappe.db.commit()
+	return True
+
+# //For Stock Entry Detail's custom field RARB Location (Source Warehouse) and RARB Location (Target Warehouse) .....
+@frappe.whitelist()
+def get_rarb_warehouse(warehouse):
+	rarb_warehouse = frappe.db.sql("""select warehouse from `tabRARB Warehouse` where warehouse = '"""+warehouse+"""' and is_active =1 and docstatus =1""", as_dict =1)
+	#print "rarb_warehouse------------",rarb_warehouse
+	if rarb_warehouse:
+		return rarb_warehouse
+	else:
+		return None
+@frappe.whitelist()
+def get_rarb_warehouse_item_name(warehouse):
+	rarb_id = frappe.db.sql("""select ri.rarb_id from `tabRARB Warehouse Item` ri , `tabRARB Warehouse` r where r.warehouse = '"""+warehouse+"""' and ri.rarb_active = "Yes" and r.name = ri.parent and r.docstatus =1 and r.is_active =1 order by ri.rarb_id asc""", as_dict=1)
+	
+	return rarb_id
+@frappe.whitelist()
+def get_rarb_items_detail(warehouse,pch_rarb_location_src):
+	rarb_items = frappe.db.sql("""select ri.rarb_item  from `tabRARB Warehouse Item` ri, `tabRARB Warehouse` r where r.warehouse = '"""+warehouse+"""' and r.name = ri.parent and r.is_active =1 and ri.rarb_id = '"""+pch_rarb_location_src+"""'""", as_dict =1)
+	return rarb_items
