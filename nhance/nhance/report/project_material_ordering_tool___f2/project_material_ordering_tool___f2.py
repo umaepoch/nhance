d87f0ce9545c2b567f8bc8d9afa17009c453af40
umaepoch <umag@epochconsulting.in>
2020-03-05 11:16:48 +0530

    PMOT

--- nhance/nhance/report/project_material_ordering_tool___f2/project_material_ordering_tool___f2.py
+++ nhance/nhance/report/project_material_ordering_tool___f2/project_material_ordering_tool___f2.py
@@ -843,7 +843,7 @@ def get_report_data(project_filter,swh_filter):
 	for rows in sum_datas:
 		if project_filter:
 			project_warehouse =  frappe.db.get_value('Project', project_filter, 'project_warehouse')
-	    		reserve_warehouse =  frappe.db.get_value('Project', project_filter, 'reserve_warehouse')
+			reserve_warehouse =  frappe.db.get_value('Project', project_filter, 'reserve_warehouse')
 			warehouse_qty = get_warehouse_qty(project_warehouse,rows[2])
 			reserve_warehouse_qty = get_warehouse_qty(reserve_warehouse,rows[2])
 			qty_consumed_in_manufacture= get_stock_entry_quantities(project_warehouse,rows[2])
@@ -1023,4 +1023,3 @@ def check_and_update(data,sreq_no):
 def getQtyAllowed(stockRequisitionID):
 	allowed_qty = frappe.db.sql("""select item_code,qty_allowed_to_be_order from `tabStock Requisition Item` where parent = %s """,stockRequisitionID, as_dict =1)
 	return allowed_qty
-
