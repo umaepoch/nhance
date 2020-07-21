f0788a52e7127f5cb13eee858d7604f13e7c8fc7
umaepoch <umaepoch@consulting.in>
2020-07-21 19:49:36 +0530

    Delivery note script updated

--- nhance/api.py
+++ nhance/api.py
@@ -1919,3 +1919,10 @@ def get_revision_no_stock(item_code,parent):
 
     return get_stock_serial_no
 
+@frappe.whitelist()
+def get_stock_qty(item_code,warehouse):
+    
+    qty = frappe.db.sql("""select concat_ws(" ", posting_date, posting_time) as date,qty_after_transaction from `tabStock Ledger Entry` where item_code='"""+item_code+"""' and warehouse='"""+warehouse+"""' order by posting_date,posting_time """, as_dict=1)
+    #print("qty",qty)
+    return qty
+
