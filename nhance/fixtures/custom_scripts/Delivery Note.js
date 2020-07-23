83665665fdcde94cd13051644a79c3520120b486
umaepoch <umaepoch@consulting.in>
2020-07-23 17:39:07 +0530

    Delivery note updated

--- nhance/fixtures/custom_scripts/Delivery Note.js
+++ nhance/fixtures/custom_scripts/Delivery Note.js
@@ -51,22 +51,22 @@ frappe.ui.form.on("Delivery Note Item", {
         var batch_no = d.batch_no;
         var revision_no = d.revision_number;
 
-        console.log("revision_no----------------" + revision_no);
+        //console.log("revision_no----------------" + revision_no);
 
 	var serial_no = d.serial_no;
 
-        console.log("revision_no----------------" + revision_no);
+        //console.log("revision_no----------------" + revision_no);
 
 
         var HasSerialNumber = null;
         HasSerialNumber = fetch_has_serial_no(item_code);
-        console.log("HasSerialNumber", HasSerialNumber);
+        //console.log("HasSerialNumber", HasSerialNumber);
         var HasBatchNumber = null;
         HasBatchNumber = fetch_has_batch_no(item_code);
-        console.log("HasBatchNumber", HasBatchNumber);
+        //console.log("HasBatchNumber", HasBatchNumber);
         var HasRevisionNumberSerial = null;
         HasRevisionNumberSerial = fetch_has_revision_number_serial(serial_no) ;
-        console.log("HasRevisionNumberSerial", HasRevisionNumberSerial);
+        //console.log("HasRevisionNumberSerial", HasRevisionNumberSerial);
 
         if ((HasRevisionNumberSerial != null || HasRevisionNumberSerial != undefined || HasRevisionNumberSerial != "") && HasSerialNumber == 1) {
             d.revision_number = HasRevisionNumberSerial;
@@ -80,7 +80,7 @@ frappe.ui.form.on("Delivery Note Item", {
 })
 
  function fetch_has_serial_no(item_code) {
-    console.log("entered into function");
+    //console.log("entered into function");
     var has_serial_no = "";
     frappe.call({
         method: 'frappe.client.get_value',
@@ -96,8 +96,8 @@ frappe.ui.form.on("Delivery Note Item", {
         callback: function(r) {
             if (r.message) {
                 has_serial_no = r.message.has_serial_no;
-                console.log(has_serial_no);
-                console.log("readings-----------" + JSON.stringify(r.message));
+                //console.log(has_serial_no);
+                //console.log("readings-----------" + JSON.stringify(r.message));
 
             }
         }
@@ -107,7 +107,7 @@ frappe.ui.form.on("Delivery Note Item", {
 
 
 function fetch_has_batch_no(item_code) {
-    console.log("entered into has_batch_no function");
+    //console.log("entered into has_batch_no function");
     var has_batch_no = "";
     frappe.call({
         method: 'frappe.client.get_value',
@@ -123,8 +123,8 @@ function fetch_has_batch_no(item_code) {
         callback: function(r) {
             if (r.message) {
                 has_batch_no = r.message.has_batch_no;
-                console.log(has_batch_no);
-                console.log("readings-----------" + JSON.stringify(r.message));
+                //console.log(has_batch_no);
+                //console.log("readings-----------" + JSON.stringify(r.message));
 
             }
         }
@@ -133,7 +133,7 @@ function fetch_has_batch_no(item_code) {
 }
 
 function fetch_has_revision_number(batch_no) {
-    console.log("entered into fetch_has_revision_number function");
+    //console.log("entered into fetch_has_revision_number function");
     var has_revision_number = "";
     frappe.call({
         method: 'frappe.client.get_value',
@@ -149,8 +149,8 @@ function fetch_has_revision_number(batch_no) {
         callback: function(r) {
             if (r.message) {
                 has_revision_number = r.message.revision_number;
-                console.log(has_revision_number);
-                console.log("readings-----------" + JSON.stringify(r.message));
+                //console.log(has_revision_number);
+                //console.log("readings-----------" + JSON.stringify(r.message));
 
             }
         }
@@ -189,7 +189,7 @@ frappe.ui.form.on("Delivery Note", "after_save", function(frm, cdt, cdn) {
     $.each(frm.doc.items, function(i, d) {
         var item_code = d.item_code;
         var revision_number = d.revision_number;
-        console.log("revision_number", revision_number);
+        //console.log("revision_number", revision_number);
         if (revision_number != "") {
             var df = frappe.meta.get_docfield("Delivery Note Item", "revision_number", cur_frm.doc.name);
 
@@ -220,12 +220,12 @@ frappe.ui.form.on("Delivery Note", "after_save", function(frm, cdt, cdn) {
         //console.log("child_received_qty", child_received_qty);
         var HasSerialNumber = null;
         HasSerialNumber = fetch_has_serial_no(item_code);
-        console.log("HasSerialNumber", HasSerialNumber);
+        //console.log("HasSerialNumber", HasSerialNumber);
 	var HasBatchNumber = null;
         HasBatchNumber = fetch_has_batch_no(item_code);
-        console.log("HasBatchNumber", HasBatchNumber);
+        //console.log("HasBatchNumber", HasBatchNumber);
 	if(HasSerialNumber == 1 ||HasBatchNumber==1){
-        console.log("item has batch or serial number make feild mandondatory");
+        //console.log("item has batch or serial number make feild mandondatory");
    	cur_frm.fields_dict.items.grid.toggle_reqd("revision_number", true)
         }
 	else{
@@ -236,7 +236,7 @@ frappe.ui.form.on("Delivery Note", "after_save", function(frm, cdt, cdn) {
 });
 
 function fetch_has_serial_no(item_code) {
-    console.log("entered into function");
+    //console.log("entered into function");
     var has_serial_no = "";
     frappe.call({
         method: 'frappe.client.get_value',
@@ -252,8 +252,8 @@ function fetch_has_serial_no(item_code) {
         callback: function(r) {
             if (r.message) {
                 has_serial_no = r.message.has_serial_no;
-                console.log(has_serial_no);
-                console.log("readings-----------" + JSON.stringify(r.message));
+                //console.log(has_serial_no);
+                //console.log("readings-----------" + JSON.stringify(r.message));
 
             }
         }
@@ -263,7 +263,7 @@ function fetch_has_serial_no(item_code) {
 
 
 function fetch_has_batch_no(item_code) {
-    console.log("entered into has_batch_no function");
+    //console.log("entered into has_batch_no function");
     var has_batch_no = "";
     frappe.call({
         method: 'frappe.client.get_value',
@@ -279,8 +279,8 @@ function fetch_has_batch_no(item_code) {
         callback: function(r) {
             if (r.message) {
                 has_batch_no = r.message.has_batch_no;
-                console.log(has_batch_no);
-                console.log("readings-----------" + JSON.stringify(r.message));
+                //console.log(has_batch_no);
+                //console.log("readings-----------" + JSON.stringify(r.message));
 
             }
         }
@@ -294,65 +294,65 @@ function fetch_has_batch_no(item_code) {
 frappe.ui.form.on("Delivery Note", "before_save", function(frm, cdt, cdn) {
     var d = locals[cdt][cdn];
     var customer = frm.doc.customer;
-    console.log("customer", customer);
+    //console.log("customer", customer);
     var company = frm.doc.company;
-    console.log("company", company);
+    //console.log("company", company);
     var items = frm.doc.items;
 
     for (var j = 0; j < items.length; j++) {
         var item_code = items[j]['item_code'];
-        console.log("item_code", item_code);
+        //console.log("item_code", item_code);
         var status = frm.doc.docstatus;
-        console.log("status", status);
+        //console.log("status", status);
         var name = frm.doc.name;
-        console.log("name", name);
+        //console.log("name", name);
         var warehouse = items[j]['warehouse'];
-        console.log("warehouse", warehouse);
+        //console.log("warehouse", warehouse);
         var posting_date1 = frm.doc.posting_date;
-        console.log("posting_date1", posting_date1);
+        //console.log("posting_date1", posting_date1);
         var posting_time1 = frm.doc.posting_time;
-        console.log("posting_time1", posting_time1);
+        //console.log("posting_time1", posting_time1);
 
         var combined_datetime = posting_date1 + " " + posting_time1;
-        console.log("combined_datetime", combined_datetime);
+        //console.log("combined_datetime", combined_datetime);
         var converted_posting_date = new Date(combined_datetime).getTime();
-        console.log("converted_posting_date", converted_posting_date);
+        //console.log("converted_posting_date", converted_posting_date);
         var available_qty_warehouse_date = fetch_qty_at_from_warehouse(item_code, warehouse);
-        console.log("available_qty_warehouse_date", available_qty_warehouse_date);
+        //console.log("available_qty_warehouse_date", available_qty_warehouse_date);
         var available_qty_warehouse_date_beforePostingDate = [];
         var available_qty_warehouse_date_beforePostingTime = [];
         var heighest_date;
         for (var z = 0; z < available_qty_warehouse_date.length; z++) {
             //var dates=available_qty_warehouse_date[z];
             available_qty_warehouse_date[z] = new Date(available_qty_warehouse_date[z]).getTime();
-            console.log("date", available_qty_warehouse_date[z], z);
+            //console.log("date", available_qty_warehouse_date[z], z);
             if (available_qty_warehouse_date[z] <= converted_posting_date) {
                 available_qty_warehouse_date_beforePostingDate.push(available_qty_warehouse_date[z]);
-                console.log("available_qty_warehouse_date_beforePostingDate", available_qty_warehouse_date_beforePostingDate);
+                //console.log("available_qty_warehouse_date_beforePostingDate", available_qty_warehouse_date_beforePostingDate);
 
             }
             z = z + 1;
         }
         var sorted_date = available_qty_warehouse_date_beforePostingDate.sort();
-        console.log("sorted_date", sorted_date);
+        //console.log("sorted_date", sorted_date);
 
         var heighest_date = sorted_date[sorted_date.length - 1];
-        console.log("heighest_date", heighest_date);
+        //console.log("heighest_date", heighest_date);
         var qty;
         for (var i = 0; i <= available_qty_warehouse_date.length; i++) {
             if (heighest_date == available_qty_warehouse_date[i]) {
                 qty = available_qty_warehouse_date[i + 1];
-                console.log("qty", qty);
+                //console.log("qty", qty);
             }
         }
 
 
         if (heighest_date != undefined) {
-            console.log("entered in if block");
+            //console.log("entered in if block");
 
             items[j]['pch_available_qty_of_transcation_at_posting_date_and_time'] = qty;
         } else {
-            console.log("entered in else block");
+            //console.log("entered in else block");
             items[j]['pch_available_qty_of_transcation_at_posting_date_and_time'] = 0;
         }
         frm.refresh_field("items")
@@ -387,35 +387,5 @@ function fetch_qty_at_from_warehouse(item_code, warehouse) {
 }
 
 
-//for testing
-frappe.ui.form.on("Delivery Note", "refresh", function(frm, cdt, cdn) {
-    var d = locals[cdt][cdn];
-    var customer = frm.doc.customer;
-    console.log("customer", customer);
-    var company = frm.doc.company;
-    console.log("company", company);
-    var items = frm.doc.items;
-    var test = fetch_from_api();
-    console.log("test", test);
-});
 
-
-function fetch_from_api() {
-    var qty_after_transaction_posting_date = "";
-    frappe.call({
-        method: 'nhance.api.testing_api',
-        async: false,
-        callback: function(r) {
-            
-       if (r.message) {
-                qty_after_transaction_posting_date = r.message;
-                console.log(qty_after_transaction_posting_date);
-                console.log("readings-----------" + JSON.stringify(r.message));
-
-            }
-
-        }
-    });
-    return qty_after_transaction_posting_date
-}
 
